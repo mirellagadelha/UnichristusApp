c05065066a9260bbebcbc941375e9172aa9e4124
Mirella Gadelha <mirella.gadelha.s@gmail.com>
2018-12-08 22:41:16 -0200

    Removed all files.

--- App/Config/AppConfig.js
+++
@@ -1,6 +0,0 @@
-const AppConfig = {
-  baseURL: "https://private-27f8f6-unichristusapp.apiary-mock.com",
-  getApiUrl: () => { return AppConfig.baseURL; },
-};
-
-export default AppConfig;
--- App/Config/index.js
+++
@@ -1,3 +0,0 @@
-import AppConfig from './AppConfig';
-
-export { AppConfig };
